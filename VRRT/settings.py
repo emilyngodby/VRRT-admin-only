23ced0de0f98ef62d519e779b432d08836af8c73
emilyngodby <egodby@nevada.unr.edu>
2021-04-25 11:50:55 -0700

    deployment

--- VRRT/settings.py
+++ VRRT/settings.py
@@ -127,8 +127,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT  =   os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
